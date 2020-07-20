d8b628d07a12061a6b4dca9594dc5ef03d0700ec
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-20 11:56:12 -0700

    Stubbed out blank moethods for Reviews Controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+    end
+
+    private
+
+    def review_params
+    end
 end
