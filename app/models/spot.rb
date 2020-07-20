eb53f79ad7ae9ae175fab42a99306a58aee851ec
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-20 00:09:32 -0700

    added basic validations to Spot model

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,4 +3,8 @@ class Spot < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
     has_one_attached :image
+    validates :not_a_duplicate
+    validates :name, presence: true
+    validates :location, presence: true
+
 end
