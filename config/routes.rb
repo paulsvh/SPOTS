e18215a8b33826ee7863ed872635794206be289f
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-20 00:31:32 -0700

    Updated Routes.rb with all 'i think' necessary routes and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,19 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
- 
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  resources :reviews
+
+  resources :spots do
+    resources :reviews, only: [:new, :index]
+  end
+
+  resources :users, only: [:show]
+
+  resources :cities, only: [:show, :index] do
+    resources :spots, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
