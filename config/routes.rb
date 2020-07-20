d4385dbb4ee772eff6bc908cb504f58a0aa0ed73
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-20 00:17:11 -0700

    removed all broad routing, will hone in routing individually

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  resources :cities
-  resources :reviews
-  resources :spots
-  resources :users
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
