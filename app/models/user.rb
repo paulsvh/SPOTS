104e0a467357427279b6d9f1eb0609fe79fa6393
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-20 00:04:12 -0700

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
     has_many :spots
     has_many :reviews
     has_many :reviewed_spots, through: :reviews, source: :spot
-    
+
+    validates :username, uniqueness:true, presence: true
+    validates :email, presence: true
+    has_secure_password
 end
