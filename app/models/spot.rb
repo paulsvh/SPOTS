7bd5fbbd25970e6b69dcd7d79a4928d854c3ee30
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-19 23:54:30 -0700

    stubbed out all associations within the 4 models

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,6 @@
 class Spot < ApplicationRecord
+    belongs_to :user
+    has_many :reviews
+    has_many :users, through: :reviews
+    has_one_attached :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+    has_many :spots
+    has_many :reviews
+    has_many :reviewed_spots, through: :reviews, source: :spot
     
 end
