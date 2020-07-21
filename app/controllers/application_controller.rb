65aa9a384c0da77f5a645b0201a06f402e026305
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-20 17:02:00 -0700

    tidied up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :require_login
-    helper_method :current_user
 
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+     end
+ 
     private
 
     def require_login
         redirect_to '/' unless session.include? :user_id
     end
 
-    def current_user
-       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    end
-
-end
+   end
