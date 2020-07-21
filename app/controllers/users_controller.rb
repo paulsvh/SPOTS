79d8bc641b3b8cdefe3459528cd9a8fb1c9812c6
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-20 19:23:25 -0700

    build functions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,17 @@ class UsersController < ApplicationController
     skip_before_action :require_login, only: [:new, :create]
 
     def new
-
+        @user = User.new
     end
 
     def create
-
+        @user = User.create(user_params)
+        session[:user_id] = @user.id
+        if session[:user_id]
+            redirect_to user_path(@user)
+        else
+            redirect_to new_user_path
+        end
     end
 
    
@@ -18,7 +24,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-
+        params.require(:user).permit(:username, :years_skating, :password, :email)
     end
     
 end
