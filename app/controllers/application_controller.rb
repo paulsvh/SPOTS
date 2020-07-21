1faa4b3b57bf42668c82eb5a24110b1898b58d42
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-20 17:00:29 -0700

    Updated validation and current_user methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,16 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :verify_authentication
+    before_action :require_login
     helper_method :current_user
 
     private
 
-    def verify_authentication
-        redirect_to '/' unless user_authenticated
-    end
-
-    def user_authenticated
-        !!current_user
+    def require_login
+        redirect_to '/' unless session.include? :user_id
     end
 
     def current_user
-        User.find_by(id: session[:user_id])
+       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
-    
+
 end
