8e76e15967ea03e4658956805158484191718643
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-22 12:39:22 -0700

    Removed creators, gave ability to create spots to User class, set associations between User and Spot.

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,7 +2,7 @@ class Spot < ApplicationRecord
     belongs_to :city
     has_many :reviews
     has_many :users, through: :reviews
-    belongs_to :creator
+    belongs_to :user
     has_one_attached :image
     validates :name, presence: true
     validates :location, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+    has_many :spots
     has_many :reviews
-    has_many :spots, through: :reviews
+    has_many :reviewed_spots, through: :reviews, source: :spot
 
     validates :username, uniqueness: true, presence: true
     validates :email, uniqueness: true, presence: true
