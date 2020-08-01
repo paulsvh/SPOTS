cbb101560aeb3d5a24daeb3ba8066bdaa8819ac8
paulsvh <pvonhagen26@gmail.com>
2020-07-31 20:49:10 -0700

    DRY'd up reviews controller by making some encompassing methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,17 +11,17 @@ class ReviewsController < ApplicationController
         if @review.save
             redirect_to spot_review_path(@review.spot, @review)
         else
-            flash[:errors] = @review.errors.full_messages
+            review_errors
             render :new
         end
     end
 
     def show
-        @review = Review.find_by(id: params[:id])
+        find_review
     end
 
     def edit
-        @review = Review.find_by(id: params[:id])
+        find_review
         if @review.user != current_user            
             flash[:errors] = "You can't edit a review you didn't create!"
             redirect_to spot_review_path(@review.spot, @review)
@@ -30,11 +30,11 @@ class ReviewsController < ApplicationController
 
 
     def update
-        @review = Review.find_by(id: params[:id])
+        find_review
         if @review.update(review_params)
             redirect_to spot_review_path(@review.spot, @review)
         else
-            flash[:errors] = @review.errors.full_messages
+            review_errors
             render :edit
         end
     end
@@ -44,5 +44,13 @@ class ReviewsController < ApplicationController
 
     def review_params
         params.require(:review).permit(:title, :content, :spot_id, :user_id)
+    end
+
+    def find_review
+        @review = Review.find_by(id: params[:id])
+    end
+
+    def review_errors
+        flash[:errors] = @review.errors.full_messages
     end
 end
