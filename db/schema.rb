bf4dd0ea4c0d2170f3d42d6113650c6de5dc21ef
paul_vonhagen <pvonhagen26@gmail.com>
2021-03-23 18:47:03 -0700

    remove db and create new with new migrations named for postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,33 +12,36 @@
 
 ActiveRecord::Schema.define(version: 2020_07_31_080359) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
+    t.text "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
+    t.text "key", null: false
+    t.text "filename", null: false
+    t.text "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.text "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "content"
-    t.string "title"
+    t.text "content"
+    t.text "title"
     t.integer "rating"
-    t.integer "user_id"
-    t.integer "spot_id"
+    t.bigint "user_id"
+    t.bigint "spot_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["spot_id"], name: "index_reviews_on_spot_id"
@@ -46,22 +49,22 @@ ActiveRecord::Schema.define(version: 2020_07_31_080359) do
   end
 
   create_table "spots", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "description"
-    t.integer "user_id"
+    t.text "name"
+    t.text "location"
+    t.text "description"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
+    t.text "username"
+    t.text "email"
+    t.text "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "uid"
+    t.text "uid"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
