253bf92b7d83592ce8f0b21af519e5ef861165a0
paul_vonhagen <pvonhagen26@gmail.com>
2021-03-23 18:14:03 -0700

    update migration syntax from t.string to t.text

--- db/migrate/20200720063345_create_spots.rb
+++ db/migrate/20200720063345_create_spots.rb
@@ -1,9 +1,9 @@
 class CreateSpots < ActiveRecord::Migration[6.0]
   def change
     create_table :spots do |t|
-      t.string :name
-      t.string :location
-      t.string :description
+      t.text :name
+      t.text :location
+      t.text :description
       t.belongs_to :user, foreign_key: true
 
       t.timestamps
