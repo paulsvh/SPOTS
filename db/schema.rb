2aaf887fb3ac09a3f0f82eb68dbcfe99c58ec5a5
paul_vonhagen <pvonhagen26@gmail.com>
2021-03-29 18:45:22 -0700

    built new postgres db and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_07_31_080359) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -37,8 +40,8 @@ ActiveRecord::Schema.define(version: 2020_07_31_080359) do
     t.string "content"
     t.string "title"
     t.integer "rating"
-    t.integer "user_id"
-    t.integer "spot_id"
+    t.bigint "user_id"
+    t.bigint "spot_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["spot_id"], name: "index_reviews_on_spot_id"
@@ -49,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_07_31_080359) do
     t.string "name"
     t.string "location"
     t.string "description"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_spots_on_user_id"
